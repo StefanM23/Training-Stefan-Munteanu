503b1c345f6d9ac91ba4c7b101e5a961fff59773
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2021-08-05 17:07:52 +0300

    {#7} {products.php} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-tefan-munteanu/tasks/7}

--- login.php
+++ login.php
@@ -1,19 +1,21 @@
 <?php 
+     session_start();
      require_once('common.php');
      require_once('config.php');
-
      if(isset($_POST['submit'])){
 
          $username=(isset($_POST['username'])) ? $_POST['username'] : "STOP";
          $password=(isset($_POST['password'])) ? $_POST['password'] : "STOP";
          
          if($username==usernameInfo && $password==passwordInfo){
+            $_SESSION['username']=$username;
+            $_SESSION['password']=$password;
             header("Location: http://localhost/ProjectMS/products.php");
             exit; 
-         }else{
-             if(!empty($username)&&!strlen($password)){
+         }else{ 
+            if(!empty($username)&&!empty($password)){
                 echo "<script>alert('The data entered is incorrect.');</script>";
-             }
+            }
          }  
      }
 ?>
@@ -29,8 +31,8 @@
 <body>
     <div class="main-login-page">
         <form action="login.php" method="post" class="myLoginPage">
-            <input type="text" name="username" placeholder="Username" ><br>
-            <input type="password" name="password" placeholder="Password"><br>
+            <input type="text" name="username" placeholder="Username" value="<?php echo isset($_POST["username"]) ? $_POST["username"] : ''; ?>" ><br>
+            <input type="password" name="password" placeholder="Password" value="<?php echo isset($_POST["password"]) ? $_POST["password"] : ''; ?>"><br>
             <input type="submit" name="submit" value="Login">
         </form>
     </div>    
--- products.php
+++ products.php
@@ -1,3 +1,28 @@
+<?php 
+    session_start();
+    require_once("common.php");
+   
+    //if logout is push
+    if(isset($_GET['logout']) & !empty($_GET['logout'])){
+        $_SESSION['username']=null;
+        $_SESSION['password']=null;
+    }
+    //the consequences of pressing the button logout redirect to login
+    if(is_null($_SESSION['username'])&& is_null($_SESSION['password'])){
+        header("Location: http://localhost/ProjectMS/login.php");
+        exit;
+    }
+    
+    if(isset($_GET['id'])){
+        $sqlDelete="DELETE FROM products WHERE id=?";
+        $result=$connection->prepare($sqlDelete);
+        if($result->execute([$_GET['id']])){
+            echo 'Product ' . $_GET['id']. ' was deleted successfully.';
+        }
+    }
+    $sql="SELECT * FROM products;";
+    $result=$connection->query($sql);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +30,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products page!</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    
+    <div class="main-section">
+        <?php while(($row=$result->fetch(PDO::FETCH_ASSOC))!==false): ?>
+            <div class="full-section-products" >
+                <div class="info-section">
+                    <img src="<?php echo $row['image'] ?>" alt="poza">
+                </div>
+                <div class="info-section">
+                    <ul>
+                        <li><?php echo $row['title'] ?></li>
+                        <li><?php echo $row['description'] ?></li>
+                        <li><?php echo $row['price'] ?>$</li>
+                    </ul>
+                </div>
+                <div class="info-section">
+                    <a href="products.php?id=">Edit</a>
+                </div>
+                <div class="info-section">
+                    <a href="products.php?id=<?php echo $row['id']; ?>">Delete</a>
+                </div>
+            </div> 
+        <?php endwhile; ?>
+        <div class="cart-section-products">
+            <a href="products.php">Add</a>
+        </div>
+        <div class="cart-section-products">
+            <a href="products.php?logout=Logout">Logout</a>
+        </div>
+    </div>
 </body>
 </html>
