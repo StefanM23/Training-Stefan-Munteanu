30218b37c300f85c42a7189eb6fd7aa430042b53
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2021-08-05 18:04:35 +0300

    {#8} {product.php} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-tefan-munteanu/tasks/8} {product.php in working}

---
+++ product.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Page</title>
+    <link rel="stylesheet" href="product.css">
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data" class="myProductFrom">
+        <input type="text" name="title" placeholder="Title"><br>
+        <input type="text" name="description" placeholder="Description"><br>
+        <input type="text" name="price" placeholder="Price"><br>
+        <input type="text" name="image" placeholder="Image">
+        <input type="file" ><br>
+        <a href="#">Products</a>
+        <input type="submit" value="Save">
+    </form>
+</body>
+</html>
