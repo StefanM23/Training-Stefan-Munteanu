c563e15c9c6f3e6b937e434daf6a18ae4a3f63ae
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2021-08-09 14:23:11 +0300

    {#5} {cart.php} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-tefan-munteanu/tasks/5}

--- cart.php
+++ cart.php
@@ -1,100 +1,82 @@
-<?php 
+<?php
 
-require_once "common.php";
-require_once "config.php";
+require_once 'common.php';
+require_once 'config.php';
 
 //prepare string HTML with products for email
-function loadHTML($image, $title, $description, $price) 
+function loadHTML($result)
 {
-    $message="<html>
+    $message = '<html>
                 <head>
                     <title>Order</title>
                 </head>
-                <body>";
-    for ($i=0; $i<count($title); ++$i) {
-        $message.="<br><b>Product $i :</b><br>
-                        <img src='{$image[$i]}' alt='poza'><br>
+                <body>';
+    foreach ($result as $product) {
+        $message .= '<br><b>Product ' . $product['title'] . ' :</b><br>
+                        <img src="' . $product['image'] . '" alt="image"><br>
                         <b>Product description:</b>
                         <ol>
-                            <li>{$title[$i]}</li>
-                            <li>{$description[$i]}</li>
-                            <li>{$price[$i]}$</li>
-                        </ol>";
+                            <li>' . $product['title'] . '</li>
+                            <li>' . $product['description'] . '</li>
+                            <li>' . $product['price'] . '</li>
+                        </ol>';
     }
-    $message.="</body></html>";
-    return  $message;            
+    $message .= '</body></html>';
+    return $message;
 }
 
 //when remove items
 if (isset($_POST['id'])) {
     $item = $_POST['id'];
     unset($_SESSION['cart'][$item]);
+    header('Location: cart.php');
+    exit;
 }
 
 //prepare sql statement for query and fetch data for the cart
 if (!empty($_SESSION['cart'])) {
-    $sql="SELECT * FROM products WHERE id IN (";
+    $strInsert = implode(',', array_fill(0, count($_SESSION['cart']), '?'));
+    $sql = 'SELECT * FROM products WHERE id IN (' . $strInsert . ');';
 
-    for ($i=0; $i<count($_SESSION['cart']); ++$i) {
-        $sql.="?,";
-    }
-    $sql=substr($sql ,0,-1);
-    $sql.=");";
-    
-    $result=$connection->prepare($sql);
-    $parameters=[];
-    foreach ($_SESSION['cart'] as $element) {
-        array_push($parameters,$element);
-    }
-    $result->execute($parameters);
-        
+    $result = $connection->prepare($sql);
+
+    $result->execute(array_values($_SESSION['cart']));
 } else {
-    $sql="SELECT * FROM products WHERE id=-1;";
-    $result=$connection->query($sql);
+    $sql = 'SELECT * FROM products WHERE id=-1;';
+    $result = $connection->query($sql);
 }
+$res = $result->fetchAll();
 
-
-$imageCart=[];
-$titleCart=[];
-$descriptionCart=[];
-$priceCart=[];
-$idCart=[];
-
-while (($row=$result->fetch(PDO::FETCH_ASSOC))!==false) {
-    array_push($imageCart,$row['image']);
-    array_push($titleCart,$row['title']);
-    array_push($descriptionCart,$row['description']);
-    array_push($priceCart,$row['price']);
-    array_push($idCart,$row['id']);
-}
 if (isset($_POST['checkout'])) {
 
-    $nameClient=$_POST['name'];
-    $nameClient=strip_tags($nameClient);
-    $addressClient=$_POST['contacts'];
-    $addressClient=strip_tags($addressClient);
-    $commentClient=$_POST['comments'];
-    $commentClient=strip_tags($commentClient);
+    $nameClient = $_POST['name'];
+    $nameClient = strip_tags($nameClient);
+    $addressClient = $_POST['contacts'];
+    $addressClient = strip_tags($addressClient);
+    $commentClient = $_POST['comments'];
+    $commentClient = strip_tags($commentClient);
 
-    if (!empty($nameClient) &&! empty($addressClient)) {
+    if (!empty($nameClient) && !empty($addressClient)) {
 
-        $header="From: <demo@stefan.me>\r\n";
-        $header.="MIME-VERSION: 1.0\r\n";
-        $header.="Content-Type: text/html; charset=ISO-8859-1\r\n";
+        $header = "From: <demo@stefan.me> \r\n";
+        $header .= "MIME-VERSION: 1.0\r\n";
+        $header .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-        $subject="Order for ".$nameClient;
+        $subject = 'Order for ' . $nameClient;
 
-        $message=" The order will be delivered to \t";
-        $message.= $addressClient." and the command is :";
-        $message.=loadHTML($imageCart,$titleCart,$descriptionCart,$priceCart);
+        $message = ' The order will be delivered to  ';
+        $message .= $addressClient . ' and the command is :';
+
+        $message .= loadHTML($res);
 
         if (!empty($commentClient)) {
-            $message.=" <b>Comments:</b>\n".$commentClient;
+            $message .= "<b>Comments:</b>\n" . $commentClient;
         }
-        mail(MANAGER_EMAIL,$subject,$message,$header);
-            
+        mail(MANAGER_EMAIL, $subject, $message, $header);
+        header('Location: cart.php');
+        exit;
     } else {
-        echo "<script>alert('You must enter your name and contacts!')</script>";
+        echo '<script>alert("You must enter your name and contacts!")</script>';
     }
 }
 ?>
@@ -104,36 +86,36 @@ if (isset($_POST['checkout'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=translateLabels("Cart Page");?></title>
+    <title><?= translateLabels('Cart Page'); ?></title>
     <link rel="stylesheet" href="/ProjectMS/index.css">
 </head>
 <body>
     <div class="main-section">
         <form action="cart.php" method="post">
-            <?php for ($i=0; $i<count($titleCart); ++$i) : ?>
+            <?php foreach ($res as $product): ?>
                 <div class="full-section">
                     <div class="info-section">
-                        <img src="<?=$imageCart[$i]; ?>" alt="<?=translateLabels("image");?>">
+                        <img src="<?= $product['image']; ?> " alt="<?= translateLabels('image'); ?>">
                     </div>
                     <div class="info-section">
                         <ul>
-                            <li><?=$titleCart[$i]; ?></li>
-                            <li><?=$descriptionCart[$i]; ?></li>
-                            <li><?=$priceCart[$i]; ?></li>
+                            <li><?= $product['title']; ?></li>
+                            <li><?= $product['description']; ?></li>
+                            <li><?= $product['price']; ?></li>
                         </ul>
                     </div>
                     <div class="info-section">
-                        <button name="id" value="<?=$idCart[$i]; ?>"><?=translateLabels("Remove");?></button>
+                        <button name="id" value="<?= $product['id']; ?>"><?= translateLabels('Remove'); ?></button>
                     </div>
                 </div>
-            <?php endfor; ?>
+            <?php endforeach; ?>
         </form>
         <form action="cart.php" method="post">
-            <input type="text" name="name" placeholder="<?=translateLabels("Name");?>" value="<?php echo isset($_POST["name"]) ? $_POST["name"] : ''; ?>"><br>
-            <textarea name="contacts" style="resize: none; id="" cols="30" rows="2" placeholder="<?=translateLabels("Contact details");?>" ><?php echo isset($_POST["contacts"]) ? $_POST["contacts"] : ''; ?></textarea><br>
-            <textarea name="comments" style="resize: none; id="" cols="30" rows="4" placeholder="<?=translateLabels("Comments");?>"><?php echo isset($_POST["comments"]) ? $_POST["comments"] : ''; ?></textarea><br>
-            <a href="index.php"><?=translateLabels("Go to Index");?></a>
-            <button name="checkout"><?=translateLabels("Checkout");?></button>
+            <input type="text" name="name" placeholder="<?= translateLabels('Name'); ?>" value="<?= isset($_POST['name']) ? $_POST['name'] : ''; ?>"><br>
+            <textarea name="contacts" style="resize: none; id="" cols="30" rows="2" placeholder="<?= translateLabels('Contact details'); ?>" ><?= isset($_POST['contacts']) ? $_POST['contacts'] : ''; ?></textarea><br>
+            <textarea name="comments" style="resize: none; id="" cols="30" rows="4" placeholder="<?= translateLabels('Comments'); ?>"><?= isset($_POST['comments']) ? $_POST['comments'] : ''; ?></textarea><br>
+            <a href="index.php"><?= translateLabels('Go to Index'); ?></a>
+            <button type="submit" name="checkout"><?= translateLabels('Checkout'); ?></button>
         </form>
     </div>
 </body>
