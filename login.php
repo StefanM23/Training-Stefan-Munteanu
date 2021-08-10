ec69015bad82bf0726dac69ba1652bdc13cc0a6a
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2021-08-10 13:18:43 +0300

    {#6} {login.php} {http://pm.5psolutions.net/public/index.php?path_info=projects/training-tefan-munteanu/tasks/6}

--- login.php
+++ login.php
@@ -1,22 +1,23 @@
-<?php 
+<?php
 
-require_once "common.php";
-require_once "config.php";
+require_once 'common.php';
+
 if (isset($_POST['submit'])) {
 
-    $username=(isset($_POST['username'])) ? $_POST['username'] : "STOP";
-    $password=(isset($_POST['password'])) ? $_POST['password'] : "STOP";
-    
-    if ($username==LOGIN_USERNAME && $password==LOGIN_PASSWORD) {
-        $_SESSION['username']=$username;
-        $_SESSION['password']=$password;
-        header("Location: http://localhost/ProjectMS/products.php");
-        exit; 
-    } else { 
-        if (!empty($username) && !empty($password)) {
-            echo "<script>alert('The data entered is incorrect.');</script>";
-        }
-    }  
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    }
+
+    if ($username == LOGIN_USERNAME && $password == LOGIN_PASSWORD) {
+        $_SESSION['username'] = $username;
+        $_SESSION['password'] = $password;
+        header('Location: products.php');
+        exit;
+    } else {
+        header('Location: login.php');
+        exit;
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -25,16 +26,16 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=translateLabels("Login Page");?></title>
+    <title><?= translateLabels('Login Page'); ?></title>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <div class="main-login-page">
         <form action="login.php" method="post" class="myLoginPage">
-            <input type="text" name="username" placeholder="<?=translateLabels("Username");?>" value="<?php echo isset($_POST["username"]) ? $_POST["username"] : ''; ?>" ><br>
-            <input type="password" name="password" placeholder="<?=translateLabels("Password");?>" value="<?php echo isset($_POST["password"]) ? $_POST["password"] : ''; ?>"><br>
-            <input type="submit" name="submit" value="<?=translateLabels("Login");?>">
+            <input type="text" name="username" placeholder="<?= translateLabels('Username'); ?>" value="<?= isset($_POST['username']) ? $_POST['username'] : ''; ?>" ><br>
+            <input type="password" name="password" placeholder="<?= translateLabels('Password'); ?>" value="<?= isset($_POST['password']) ? $_POST['password'] : ''; ?>"><br>
+            <input type="submit" name="submit" value="<?=translateLabels('Login');?>">
         </form>
-    </div>    
+    </div>
 </body>
 </html>
